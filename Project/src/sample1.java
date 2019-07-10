bf0dca8ed067f470637381bc209bf902757115a0
mio2 <motty.mio2@gmail.com>
2019-07-10 11:17:04 +0800

    final
    目標まで一旦完成
    色別点数機能 ver1

--- Project/src/sample1.java
+++ Project/src/sample1.java
@@ -21,18 +21,20 @@ public class sample1 /* implements /*ActionListener */ {
 
     public static int gamemode = 0;
     public static int score;
+    public static int highscore = 0;
+    public static int targetmng = 0;
+    public static int[] scorecng = { 0, 0 };
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setTitle("Simple Game1");
-        int wi = 600;
-        int hi = 800;
+        int wi = 900;
+        int hi = 1000;
         int windowW = wi - 16;
         int windowH = hi - 39;
         frame.setSize(wi, hi);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // frame.getContentPane().setBackground(new Color(224,223,229,250));
         frame.getContentPane().setBackground(Color.LIGHT_GRAY);
         frame.setLayout(null);
 
@@ -40,12 +42,16 @@ public class sample1 /* implements /*ActionListener */ {
 
         JTextField timer = new JTextField(50);
         timer.setLocation(0, windowW);
-        timer.setSize(windowW * 2 / 3, windowH - windowW);
-        // timer.setFont(,default,defau24);
-
-        timer.setText("Let's Challenge !!");
+        timer.setSize(windowW / 3, windowH - windowW);
         timer.setHorizontalAlignment(JTextField.RIGHT);
         frame.add(timer);
+
+        JTextField hscoremng = new JTextField(50);
+        hscoremng.setLocation(windowW / 3, windowW);
+        hscoremng.setSize(windowW * 1 / 3, windowH - windowW);
+        hscoremng.setText("High Score \n" + highscore);
+        hscoremng.setHorizontalAlignment(JTextField.RIGHT);
+        frame.add(hscoremng);
 
         JTextField scoreboard = new JTextField(50);
         scoreboard.setLocation(windowW * 2 / 3, windowW);
@@ -54,11 +60,16 @@ public class sample1 /* implements /*ActionListener */ {
         scoreboard.setHorizontalAlignment(JTextField.RIGHT);
         frame.add(scoreboard);
 
-        ActionListener listen = new MyButtonListener1(scoreboard);
+        ActionListener listen = new MyButtonListener1();
+        ActionListener listen2 = new MyButtonListener2();
 
         JButton btn1 = new JButton();
         btn1.addActionListener(listen);
         frame.add(btn1);
+
+        JButton btn2 = new JButton();
+        btn2.addActionListener(listen2);
+        frame.add(btn2);
 
         JButton btnstt = new JButton();
         btnstt.addActionListener(new Modechanger());
@@ -72,69 +83,172 @@ public class sample1 /* implements /*ActionListener */ {
         frame.setLayout(null);
         frame.setVisible(true);
 
+        btn1.setFont(new Font("Arial", Font.PLAIN, 20));
+        btn2.setFont(new Font("Arial", Font.PLAIN, 20));
         timer.setFont(new Font("Arial", Font.PLAIN, 30));
+        hscoremng.setFont(new Font("Arial", Font.PLAIN, 30));
         scoreboard.setFont(new Font("Arial", Font.PLAIN, 30));
 
-        int targS, targX, targY;
+        int[] targS = new int[2];
+        int[] targX = new int[2];
+        int[] targY = new int[2];
+        long[] flgT = new long[2];
         long staT;
         long nowT;
-        long flgT;
         int oldscore;
+        JButton[] btnlist = { btn1, btn2 };
 
         while (true) {
             if (gamemode == 1) {
                 score = 0;
                 oldscore = score;
                 scoreboard.setText(String.valueOf(score));
+                btnstt.setLocation(windowH, windowH);
+                for (int i = 0; i < 2; i++) { // start up script
+                    targS[i] = rand.nextInt(40) + 80;
+                    targX[i] = rand.nextInt((windowW - targS[i]) / 2) * (i + 1);
+                    targY[i] = rand.nextInt(windowW - targS[i]);
+                    int setscore;
+                    setscore = rand.nextInt(8) + 1;
+                    btnlist[i].setText(String.valueOf(setscore));
+                    scorecng[i] = rand.nextInt(9);
+                    if (scorecng[i] == 0) {
+                        btnlist[i].setBackground(Color.CYAN);
+                    } else if (scorecng[i] == 7) {
+                        btnlist[i].setBackground(Color.RED);
+                    } else {
+                        btnlist[i].setBackground(Color.WHITE);
+                    }
+                    btnlist[i].setLocation(targX[i], targY[i]);
+                    btnlist[i].setSize(targS[i], targS[i]);
+                    flgT[i] = System.currentTimeMillis();
+                }
                 staT = System.currentTimeMillis();
                 nowT = System.currentTimeMillis();
-                flgT = System.currentTimeMillis();
-                btnstt.setLocation(windowH, windowH);
-                btn1.setLocation(windowH, windowH);
                 while (nowT - staT < 30000) {
                     gamemode = 1;
                     nowT = System.currentTimeMillis();
                     timer.setText("Last " + (30 - (nowT - staT) / 1000) + "s");
 
-                    if (System.currentTimeMillis() - flgT > 1000 || oldscore != score) {
-                        oldscore = score;
+                    if (targetmng == 0) {
+                        if (nowT - flgT[0] > 1000) {
+                            targetmng += 1;
+                        }
+                        if (nowT - flgT[1] > 1000) {
+                            targetmng += 2;
+                        }
+                    }
+                    System.out.println(targetmng);
+
+                    if (targetmng == 1 || targetmng == 2) {
+                        int i = targetmng - 1;
                         do {
-                            targS = rand.nextInt(150);
-                        } while (targS < 30);
-                        targX = rand.nextInt(windowW - targS);
-                        targY = rand.nextInt(windowW - targS);
+                            targS[i] = rand.nextInt(120);
+                        } while (targS[i] < 80);
+                        targX[i] = rand.nextInt((windowW - targS[i]) / 2) * (i + 1);
+                        targY[i] = rand.nextInt(windowW - targS[i]);
                         int setscore;
                         do {
                             setscore = rand.nextInt(9);
                         } while (setscore == 0);
-                        btn1.setText(String.valueOf(setscore));
-                        btn1.setLocation(targX, targY);
-                        btn1.setSize(targS, targS);
-                        flgT = System.currentTimeMillis();
+                        scorecng[i] = rand.nextInt(9);
+                        if (scorecng[i] == 0) {
+                            btnlist[i].setBackground(Color.CYAN);
+                        } else if (scorecng[i] == 7) {
+                            btnlist[i].setBackground(Color.RED);
+                        } else {
+                            btnlist[i].setBackground(Color.WHITE);
+                        }
+                        btnlist[i].setText(String.valueOf(setscore));
+                        btnlist[i].setLocation(targX[i], targY[i]);
+                        btnlist[i].setSize(targS[i], targS[i]);
+                        flgT[i] = System.currentTimeMillis();
+                    } else if (targetmng == 3) {
+                        for (int i = 0; i < 2; i++) { // start up script
+                            targS[i] = rand.nextInt(40) + 80;
+                            targX[i] = rand.nextInt((windowW - targS[i]) / 2) * (i + 1);
+                            targY[i] = rand.nextInt(windowW - targS[i]);
+                            int setscore;
+                            setscore = rand.nextInt(8) + 1;
+                            scorecng[i] = rand.nextInt(9);
+                            if (scorecng[i] == 0) {
+                                btnlist[i].setBackground(Color.CYAN);
+                            } else if (scorecng[i] == 7) {
+                                btnlist[i].setBackground(Color.RED);
+                            } else {
+                                btnlist[i].setBackground(Color.WHITE);
+                            }
+                            btnlist[i].setText(String.valueOf(setscore));
+                            btnlist[i].setLocation(targX[i], targY[i]);
+                            btnlist[i].setSize(targS[i], targS[i]);
+                            flgT[i] = System.currentTimeMillis();
+                        }
+                    }
+
+                    targetmng = 0;
+
+                    if (oldscore != score) {
+                        scoreboard.setText(String.valueOf(score));
+
+                        oldscore = score;
+                    }
+                    if (score >= highscore) {
+                        highscore = score;
+                        hscoremng.setText("High Score \n" + highscore);
+                        scoreboard.setBackground(Color.ORANGE);
+                        hscoremng.setBackground(Color.ORANGE);
+                    } else {
+                        scoreboard.setBackground(Color.WHITE);
+                        hscoremng.setBackground(Color.WHITE);
                     }
                 }
                 gamemode = 0;
             } else {
+                score = 0;
+                timer.setText("Let's Challenge !!");
                 btnstt.setLocation((windowW - sttw) / 2, (windowW - stth) / 2);
                 btn1.setLocation(windowH, windowH);
+                btn2.setLocation(windowH, windowH);
             }
         }
     }
 }
 
 public class MyButtonListener1 implements ActionListener {
-    private JTextField scoreboard;
-
-    public MyButtonListener1(JTextField scoreget) {
-        this.scoreboard = scoreget;
-    }
-
     public void actionPerformed(ActionEvent e) {
         String command = e.getActionCommand();
-        // score += Integer.parseInt(command);
+        int rate;
+        switch (sample1.scorecng[0]) {
+        case 0:
+            rate = -1;
+            break;
+        case 7:
+            rate = 2;
+            break;
+        default:
+            rate = 1;
+        }
+        sample1.score += Integer.parseInt(command) * rate;
+        sample1.targetmng = 1;
+    }
+}
 
-        sample1.score += Integer.parseInt(command);
-        scoreboard.setText(String.valueOf(sample1.score));
+public class MyButtonListener2 implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        int rate;
+        switch (sample1.scorecng[1]) {
+        case 0:
+            rate = -1;
+            break;
+        case 7:
+            rate = 2;
+            break;
+        default:
+            rate = 1;
+        }
+        sample1.score += Integer.parseInt(command) * rate;
+        sample1.targetmng = 2;
     }
 }
 
